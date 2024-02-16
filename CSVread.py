d87e0ea23da2d55412ca910086dd60a481675984
Chowdhury <llj9007@sis.nyp.org>
2024-02-16 10:51:37 -0500

    new change

--- CSVread.py
+++ CSVread.py
@@ -1,28 +1,26 @@
-import csv  #module is used for read write and investiagete csv 
-"""
-Read and print rows from csv file
+import csv  # Module used for reading and writing CSV files.
 
-for argument it takes the name of the csv file.
-"""
+def read_csv(filename):
+    """
+    Read and print rows from a CSV file.
 
-def read_csv(filename): #giving name of the func and argument name
-    try:
-        with open(filename, 'r', newline='') as file: #with is an statement it access resources likes files
-            """open is and function that pen files, and it takes atleast one argument 'filename' which will be
-            the csv file i want to open . the 'r' tells us that csv file will me open and will be in read mode.
-            the newline paramater will understand when we go to a newline in the csv file.
-            """
-            csv_reader = csv.reader() # assigning the rading function of csv to csv_reader variable.
-            next(csv_reader) #skips the first line
-            for row in csv_reader:  #this for loop prints out the rows one by one
-                print(', '.join(row)) #join each rows words with a comma in between
-    except:
-        print("Something wrong with the code")  #if code wrong this will print
+    Args:
+        filename (str): The name of the CSV file to read.
+    """
+    with open(filename, 'r', newline='') as file:
+        """
+        Open the CSV file in read mode ('r').
+        The 'newline' parameter ensures universal newline support.
+        """
+        csv_reader = csv.reader(file)  # Create a CSV reader object.
+        next(csv_reader)  # Skip the first line (header).
+        for row in csv_reader:  # Iterate over each row in the CSV file.
+            print(', '.join(row))  # Join each row's words with a comma and print.
 
-
-if __name__ == "__main__": 
-    '''if I running code here, the if statment will run or else the if statement
-    won't run as I will be suing it as an helper from another program.
-    '''
-    csv_filename = "names.csv" # saving the name of the csv file in a variable
-    read_csv(csv_filename) #read_csv takes the argument and reads the CSV file
+if __name__ == "__main__":
+    """
+    If this script is run directly (not imported as a module),
+    execute the code below.
+    """
+    csv_filename = "names.csv"  # Name of the CSV file.
+    read_csv(csv_filename)  # Call the function to read the CSV file.
